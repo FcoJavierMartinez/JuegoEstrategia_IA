f0a41e42181b114f2a9de508b7fdde7d3fe2d054
Anabel Torner <al394504@uji.es>
2022-12-09 20:27:14 +0100

    invocacionAleatoria y arreglos

--- Assets/Scripts/buttonScript.cs
+++ Assets/Scripts/buttonScript.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class buttonScript : MonoBehaviour
 {
@@ -14,48 +13,88 @@ public class buttonScript : MonoBehaviour
     public int gastoEnergiaMago;
     public int gastoEnergiaArquero;
 
+    public GameObject bomba;
+    public GameObject tanque;
+    public GameObject mele;
+    public GameObject mago;
+    public GameObject arquero;
+
+    public GameObject posicionTorreCentral;
+    public GameObject posicionTorreIzq;
+    public GameObject posicionTorreDer;
+
     //Bomba
-    public void gastarEnergiaBomba()
+    public void invocarBomba()
     {
         if (energy.energy > gastoEnergiaBomba)
         {
             energy.energy -= gastoEnergiaBomba;
+            switch(Random.Range(0, 3))
+            {
+                case 0: Instantiate(bomba, posicionTorreCentral.transform); break;
+                case 1: Instantiate(bomba, posicionTorreIzq.transform); break;
+                case 2: Instantiate(bomba, posicionTorreDer.transform); break;
+            }
         }
     }
 
     //Tanque
-    public void gastarEnergiaTanque()
+    public void invocarTanque()
     {
         if (energy.energy > gastoEnergiaTanque)
         {
             energy.energy -= gastoEnergiaTanque;
+            switch (Random.Range(0, 3))
+            {
+                case 0: Instantiate(tanque, posicionTorreCentral.transform); break;
+                case 1: Instantiate(tanque, posicionTorreIzq.transform); break;
+                case 2: Instantiate(tanque, posicionTorreDer.transform); break;
+            }
         }
     }
 
     //Mele
-    public void gastarEnergiaMele()
+    public void invocarMele()
     {
         if (energy.energy > gastoEnergiaMele)
         {
             energy.energy -= gastoEnergiaMele;
+            switch (Random.Range(0, 3))
+            {
+                case 0: Instantiate(mele, posicionTorreCentral.transform); break;
+                case 1: Instantiate(mele, posicionTorreIzq.transform); break;
+                case 2: Instantiate(mele, posicionTorreDer.transform); break;
+            }
         }
     }
 
     //Mago
-    public void gastarEnergiaMago()
+    public void invocarMago()
     {
         if (energy.energy > gastoEnergiaMago)
         {
             energy.energy -= gastoEnergiaMago;
+            switch (Random.Range(0, 3))
+            {
+                case 0: Instantiate(mago, posicionTorreCentral.transform); break;
+                case 1: Instantiate(mago, posicionTorreIzq.transform); break;
+                case 2: Instantiate(mago, posicionTorreDer.transform); break;
+            }
         }
     }
 
     //Arquero
-    public void gastarEnergiaArquero()
+    public void invocarArquero()
     {
         if (energy.energy > gastoEnergiaArquero)
         {
             energy.energy -= gastoEnergiaArquero;
+            switch (Random.Range(0, 3))
+            {
+                case 0: Instantiate(arquero, posicionTorreCentral.transform); break;
+                case 1: Instantiate(arquero, posicionTorreIzq.transform); break;
+                case 2: Instantiate(arquero, posicionTorreDer.transform); break;
+            }
         }
     }
 }
